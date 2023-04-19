70897d17e7c89d33db1232d7579043ce027a9837
prateekchhibber <0392pc@gmail.com>
2023-04-19 23:34:59 +0530

    Receiving Book Titles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { useState } from 'react'
+import BookCreate from './components/BookCreate'
+
 function App (){
-    return <div>App</div>
+    // It will display all books at a given time
+    const [books, setBooks] = useState([])
+
+    const handleCreateBook = (title) => {
+        console.log('Need to add book with:',title)
+    }
+    
+    return <div>
+         <BookCreate onCreate={handleCreateBook} />
+    </div>
 }
 
 export default App
