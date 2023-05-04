164c8b4d427ad7f8941f38b271c3f54919afacae
prateekchhibber <0392pc@gmail.com>
2023-05-05 00:32:30 +0530

    Added Context to the app

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,14 @@
-import { useContext } from 'react'
-import BooksContext from '../context/books'
-import BookShow from './BookShow'
+import { useContext } from "react";
+import BooksContext from "../context/books";
+import BookShow from "./BookShow";
 
-function BookList({ books, onDelete, onEdit }) {
-    const value = useContext(BooksContext)
-    const renderedBooks = books.map((book) => {
-        return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
-    })
-    return <div className='book-list'>
-        {value}
-        {renderedBooks}</div>
+function BookList() {
+  const { books } = useContext(BooksContext);
+
+  const renderedBooks = books.map((book) => {
+    return <BookShow key={book.id} book={book} />;
+  });
+  return <div className="book-list">{renderedBooks}</div>;
 }
 
-export default BookList
+export default BookList;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import BooksContext from './context/books';
+import {Provider} from './context/books';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BooksContext.Provider value={5}>
+    <Provider>
       <App />
-    </BooksContext.Provider>
+    </Provider>
   </React.StrictMode>
 );
 
