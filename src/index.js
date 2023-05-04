2e0bc8f9f63f7a68d4ff5cf22bd5c57932e6afe6
prateekchhibber <0392pc@gmail.com>
2023-05-04 19:39:08 +0530

    Adding Context

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App'
+import App from './App';
+import BooksContext from './context/books';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BooksContext.Provider value={5}>
+      <App />
+    </BooksContext.Provider>
   </React.StrictMode>
 );
 
